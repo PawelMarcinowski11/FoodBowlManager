8467dab8e5ef8299c4df48cdeaa26d04e9cd46d0
pawel.marcinowski11 <pawel.marcinowski11@gmail.com>
2021-11-16 09:49:55 +0100

    improve ocr-based autocomplete

--- app/src/main/java/marcinowski/pawel/foodmanager/ImageProcessing.kt
+++ app/src/main/java/marcinowski/pawel/foodmanager/ImageProcessing.kt
@@ -1,8 +1,12 @@
 package marcinowski.pawel.foodmanager
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.SurfaceTexture
 import android.view.TextureView
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
@@ -11,16 +15,17 @@ import kotlin.concurrent.thread
 
 class ImageProcessing(
     private var textureViewRef: TextureView?,
-    productParameters: ProductParameters
+    productParameters: ProductParameters,
+    context: Context
 ) {
 
 
-    private var isProcessing : Semaphore
+    private var isProcessingText : Semaphore = Semaphore(1)
+    private var isProcessingBarcode : Semaphore = Semaphore(1)
 
     var textureListener: TextureView.SurfaceTextureListener
 
     init {
-        this.isProcessing = Semaphore(1)
         this.textureListener = object :
             TextureView.SurfaceTextureListener {
             override fun onSurfaceTextureAvailable(
@@ -44,48 +49,113 @@ class ImageProcessing(
             }
 
             override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {
-                if (isProcessing.tryAcquire()) {
-                    thread {
-                        if (textureViewRef != null) {
-                            val frame = Bitmap.createBitmap(
-                                textureViewRef!!.width,
-                                textureViewRef!!.height,
-                                Bitmap.Config.ARGB_8888
-                            )
+                if (productParameters.autoComplete.value == true
+                    && (isProcessingText.availablePermits > 0
+                            || isProcessingBarcode.availablePermits > 0)) {
 
-                            val bitmap = textureViewRef!!.getBitmap(frame)
+                    if (textureViewRef != null) {
+                        val frame = Bitmap.createBitmap(
+                            textureViewRef!!.width,
+                            textureViewRef!!.height,
+                            Bitmap.Config.ARGB_8888
+                        )
 
-//                            val options = BarcodeScannerOptions.Builder()
-//                                .setBarcodeFormats(
-//                                    Barcode.FORMAT_QR_CODE,
-//                                    Barcode.FORMAT_AZTEC)
-//                                .build()
+                        val bitmap = textureViewRef!!.getBitmap(frame)
 
-                            val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-                            val result = recognizer.process(InputImage.fromBitmap(bitmap, 0))
-                                .addOnSuccessListener { visionText ->
-                                    //productParameters.productName.value = visionText.text
+                        val inputImage = InputImage.fromBitmap(bitmap, 0)
 
-                                    var datePattern = Regex("""[0-3]\d[.][0-1]\d[.][2][0]\d\d""")
-                                    var trimmedResult = datePattern.find(visionText.text)?.value
+                        if (isProcessingText.tryAcquire()) {
+                            thread {
 
-                                    if(trimmedResult != null)
-                                        productParameters.productName.value = trimmedResult
+                                val textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+                                textRecognizer.process(inputImage)
+                                    .addOnSuccessListener { visionText ->
+                                        val ddmmyyyyPattern = """[0-3]\d[.,\/](([0]\d)|([1][0-2]))[.,\/][2][0]\d\d"""
+                                        val ddmmyyPattern = """[0-3]\d[.,\/](([0]\d)|([1][0-2]))[.,\/]\d\d"""
+                                        val mmyyyyPattern = """^(([0]\d)|([1][0-2]))[.,\/][2][0]\d\d"""
+                                        val mmyyPattern = """^(([0]\d)|([1][0-2]))[.,\/]\d\d"""
+
+                                        if (Regex(ddmmyyyyPattern).find(visionText.text)?.value != null) {
+                                            val expiryDate = Regex(ddmmyyyyPattern).find(visionText.text)?.value!!
+                                            productParameters.expiryDate.value = expiryDate
+                                        }
+                                        else if (Regex(ddmmyyPattern).find(visionText.text)?.value != null) {
+                                            val expiryDate = Regex(ddmmyyPattern).find(visionText.text)?.value!!
+                                            productParameters.expiryDate.value = expiryDate.substring(0,6) + "20" + expiryDate.substring(6)
+                                        }
+                                        else if (Regex(mmyyyyPattern).find(visionText.text)?.value != null) {
+                                            val expiryDate = Regex(mmyyyyPattern).find(visionText.text)?.value!!
+                                            if (expiryDate.contains('.'))
+                                                productParameters.expiryDate.value = "01." + expiryDate
+                                            else if (expiryDate.contains('/'))
+                                                productParameters.expiryDate.value = "01/" + expiryDate
+                                        }
+                                        else if (Regex(mmyyPattern).find(visionText.text)?.value != null) {
+                                            val expiryDate = Regex(mmyyPattern).find(visionText.text)?.value!!
+                                            if (expiryDate.contains('.'))
+                                                productParameters.expiryDate.value = "01." + expiryDate.substring(0,3) + "20" + expiryDate.substring(3)
+                                            else if (expiryDate.contains('/'))
+                                                productParameters.expiryDate.value = "01/" + expiryDate.substring(0,3) + "20" + expiryDate.substring(3)
+
+                                        }
+
+                                        isProcessingText.release()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        isProcessingText.release()
+                                    }
 
 
-                                    //var xd2 = visionText.textBlocks[0]
-                                    // Task completed successfully
-                                    // ...
-                                    isProcessing.release()
-                                }
-                                .addOnFailureListener { e ->
-                                    // Task failed with an exception
-                                    // ...
-                                    isProcessing.release()
-                                }
+                            }
+                        }
 
+                        if (isProcessingBarcode.tryAcquire()) {
+                            thread {
+
+
+                                val options = BarcodeScannerOptions.Builder()
+                                    .setBarcodeFormats(
+                                        Barcode.FORMAT_EAN_13,
+                                        Barcode.FORMAT_EAN_8,
+                                        Barcode.FORMAT_UPC_A,
+                                        Barcode.FORMAT_UPC_E,
+                                    )
+                                    .build()
+
+                                val barcodeRecognizer = BarcodeScanning.getClient(options)
+                                barcodeRecognizer.process(inputImage)
+                                    .addOnSuccessListener { barcodes ->
+
+                                        if(barcodes.isNotEmpty() && productParameters.barcodeNumber.value !=
+                                                barcodes[0].displayValue!!) {
+                                            productParameters.barcodeNumber.value =
+                                                barcodes[0].displayValue!!
+
+
+                                            lookUpProductName(
+                                                productParameters.barcodeNumber.value,
+                                                productParameters.productName,
+                                                context
+                                            );
+
+                                        }
+
+
+
+
+                                        isProcessingBarcode.release()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        // Task failed with an exception
+                                        // ...
+                                        isProcessingBarcode.release()
+                                    }
+
+                            }
                         }
                     }
+
+
                 }
             }
         }
---
+++ app/src/main/java/marcinowski/pawel/foodmanager/ProcessData.kt
@@ -0,0 +1,28 @@
+package marcinowski.pawel.foodmanager
+
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.MutableState
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.net.URL
+import kotlin.concurrent.thread
+
+
+fun lookUpProductName (barcode: String, productName: MutableState<String>, context: Context) {
+    thread {
+        try {
+            val jsonFile =
+                URL("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json?fields=product_name").readText()
+            val jsonObject = JSONTokener(jsonFile).nextValue() as JSONObject
+            productName.value = jsonObject.getJSONObject("product").getString("product_name")
+        } catch (e: Exception) {
+            (context as Activity).runOnUiThread {
+                Toast.makeText(context, context.getResources().getString(R.string.toast_product_not_found), Toast.LENGTH_SHORT).show()
+            }
+            e.printStackTrace()
+        }
+    }
+
+}
